69322320e67004175af15715deb877c73f0e57b7
Dxcraig <steviedoe20@gmail.com>
2025-01-21 19:34:41 +0000

    Added getters and setters for doctor fields

--- src/main/java/org/example/Doctor.java
+++ src/main/java/org/example/Doctor.java
@@ -11,4 +11,36 @@ public class Doctor {
         this.name = name;
         this.specialization = specialization;
     }
+
+    public String getDoctorId() {
+        return doctorId;
+    }
+
+    public void setDoctorId(String doctorId) {
+        this.doctorId = doctorId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSpecialization() {
+        return specialization;
+    }
+
+    public void setSpecialization(String specialization) {
+        this.specialization = specialization;
+    }
+
+    public int getPatientsTreated() {
+        return patientsTreated;
+    }
+
+    public void setPatientsTreated(int patientsTreated) {
+        this.patientsTreated = patientsTreated;
+    }
 }
