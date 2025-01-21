97a01ecdac6e367e7824b2551fc3ad8eb4e519a2
Dxcraig <steviedoe20@gmail.com>
2025-01-21 19:43:20 +0000

    Modified main method

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,26 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Patient patient1 = new Patient("P001", "John Smith", 45, "Fever");
+        Doctor doctor1 = new Doctor("D101", "Dr. Alice", "General Medicine");
+
+
+        try {
+            patient1.setAge(-5);
+        } catch (IllegalArgumentException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+
+        try {
+            patient1.setDiagnosis("");
+        } catch (IllegalArgumentException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+
+
+
+        patient1.updateDiagnosis("Flu");
+        doctor1.treatPatient();
+
     }
 }
